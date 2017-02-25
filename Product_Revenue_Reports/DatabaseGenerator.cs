1dd4582bb1d77538b0e4c8223369c2a87683bcfc
Kate <katerebekah@gmail.com>
2017-02-25 10:28:33 -0600

    moved to .net framework

---
+++ Product_Revenue_Reports/DatabaseGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Revenue_Reports
+{
+    public class DatabaseGenerator
+    {
+        Random rnd = new Random();
+
+        string[] customers = new[] { "Carys", "Emmett", "Latoya", "Trina", "Kade", "Torin", "Aggie", "Caelan", "Patsy", "Bettina", "Hans", "Leda", "Clair", "Evan", "Roscoe", "Sondra", "Dixon", "Gail" };
+        string[] customersLastName = new[] { "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor", "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin" };
+        string[] products = new[] { "Rug", "Wine Glasses", "Book Ends", "Picture Frame", "Blu-Ray Player", "Digital Camera", "Stuffed Animal - Monkey", "Stuffed Animal - Sloth", "Spatula", "Crayons", "Headphones", "Lawn Furniture", "Hammer", "Computer Monitor", "Golf Clubs", "Raspberry Pi", "eReader" };
+        int[] productprice = new[] { 57, 21, 16, 22, 95, 257, 4, 5, 10, 2, 53, 150, 25, 950, 860, 45, 250 };
+        int[] productrevenue = new[] { 3, 1, 4, 2, 15, 52, 1, 1, 1, 1, 5, 53, 3, 24, 169, 10, 9 };
+        string[] customerAddressNumbers = new[] { "123", "435", "44", "283a", "6b", "1440", "7723", "289", "7564", "985-b", "33922", "23", "546", "5692", "6780", "9362", "121", "74567", "18", "9" };
+        string[] customerAddressStreet = new[] { "Mallory Lane", "Carothers Pkwy", "Claybrook Lane", "Bending Creek Drive", "Old Hickory Blvd", "Harris Ave", "21st Ave N", "Plus Park Blvd", "Interstate Blvd S", "Whitney Ave", "Bell Rd", "Harding Pky", "Nolesville Road", "Charlotte Ave" };
+        int[] customerZipcode = new int[] { 37013, 37072, 38461, 37115, 37116, 37201, 37211, 37216, 37222 };
+        string[] supplierState = new string[] { "AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HA", "ID", "IL", "IN", "IA", "KA", "KY", "LA", "ME", "MD", "MS", "MC", "MN", "MI", "MO", "MT", "NB", "NV", "NH", "NJ", "NC", "NY", "NM", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "WA", "WV", "WI", "WY" };
+        public string RandomizeCustomerProducts()
+        {
+            var rnd1 = rnd.Next(customers.Length);
+            var rnd2 = rnd.Next(customersLastName.Length);
+            var rnd3 = rnd.Next(products.Length);
+            var rnd4 = rnd.Next(customerAddressNumbers.Length);
+            var rnd5 = rnd.Next(customerAddressStreet.Length);
+            var rnd6 = rnd.Next(customerZipcode.Length);
+            var rnd7 = rnd.Next(supplierState.Length);
+
+            DateTime start = DateTime.Today.AddDays(-200);
+            int range = (DateTime.Today - start).Days;
+
+            return string.Format("INSERT INTO [dbo].[Revenue] VALUES('{0}', {1}, {2}, '{3}', '{4}', '{5}', '{6} {7}', {8}, '{9}') ",
+                products[rnd3], productprice[rnd3], productrevenue[rnd3], supplierState[rnd7], customers[rnd1], customersLastName[rnd2], customerAddressNumbers[rnd4], customerAddressStreet[rnd5], customerZipcode[rnd6], start.AddDays(rnd.Next(range)));
+        }
+
+        public string RandomizeCustomerProducts(int numOfEntries)
+        {
+            string returnstring = "";
+            for (var i = 0; i < numOfEntries; i++) { returnstring += RandomizeCustomerProducts(); }
+            return returnstring;
+        }
+
+        public void CreateDatabase()
+        {
+            string sql1 = "USE Master " +
+                            "IF EXISTS (" +
+                            "SELECT name FROM sys.databases WHERE name = N'BangProductRevenueReports' " +
+                            ") DROP DATABASE BangProductRevenueReports ";
+            string sql2 = "Use Master " +
+                            "CREATE DATABASE BangProductRevenueReports ";
+            string sql3 = "Use BangProductRevenueReports " +
+                            "CREATE TABLE [dbo].[Revenue] (" +
+                                "[Id] INT PRIMARY KEY NOT NULL IDENTITY, " +
+                                "[ProductName] varchar(50) NOT NULL, " +
+                                "[ProductCost] int not null," +
+                                "[ProductRevenue] int not null, " +
+                                "[ProductSupplierState] varchar(10) not null, " +
+                                "[CustomerFirstName] varchar(50) not null, " +
+                                "[CustomerLastName] varchar(50) not null, " +
+                                "[CustomerAddress] varchar(100) not null, " +
+                                "[CustomerZipCode] int not null, " +
+                                "[PurchaseDate] Datetime not null " +
+                            ") "
+                            + RandomizeCustomerProducts(1000);
+
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB");
+            using (connection)
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sql1, connection);
+                SqlCommand command2 = new SqlCommand(sql2, connection);
+                SqlCommand command3 = new SqlCommand(sql3, connection);
+                command.ExecuteNonQuery();
+                command2.ExecuteNonQuery();
+                command3.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- Product_Revenue_Reports/Program.cs
+++ Product_Revenue_Reports/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,216 @@ namespace Product_Revenue_Reports
     {
         static void Main(string[] args)
         {
+
+            //Comment out these two lines for speed purposes after the initial db creation 
+            //Uncomment them and run to generate fresh data
+            DatabaseGenerator gen = new DatabaseGenerator();
+            gen.CreateDatabase();
+
+            SqlCommand cs = new SqlCommand();
+            cs.Connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB");
+            cs.CommandType = CommandType.Text;
+            SqlDataReader reader;
+
+            //List<string> Names = new List<string>();
+            //List<string> Values = new List<string>()
+            List<KeyValuePair<string, int>> reportValues = new List<KeyValuePair<string, int>>();
+
+            Console.WriteLine("Bangazon Reports");
+            bool go_on = true;
+
+            while (go_on)
+            {
+                try
+                {
+
+                    Console.WriteLine("1 - Last Week Report");
+                    Console.WriteLine("2 - Last Month Report");
+                    Console.WriteLine("3 - Last 3 months Report");
+                    Console.WriteLine("4 - Rev by customer");
+                    Console.WriteLine("5 - Rev by product");
+
+                    var stuff = Console.ReadLine();
+
+                    switch (stuff)
+                    {
+                        case "1":
+                            cs.CommandText = "SELECT * FROM [BangProductRevenueReports].[dbo].[Revenue]";
+                            cs.Connection.Open();
+                            reader = cs.ExecuteReader();
+                            //var proDict = new Dictionary<string, int>();
+                            while (reader.Read())
+                            {
+                                var i = reader[1];
+                                var hh = i.ToString();
+                                var a = reader[3];
+                                var t = a.ToString();
+                                var e = int.Parse(t);
+                                var r = reader[9];
+                                //  e
+                                var p = r.ToString();
+                                var o = DateTime.Parse(p);
+                                //  r
+                                //  t
+                                var s = DateTime.Today.AddDays(-7);
+                                var straightupbull = new KeyValuePair<string, int>(hh, e);
+                                if (o > s)
+                                {
+                                    //proDict.Add(h, e); throws error GRRRRRR      
+                                    reportValues.Add(straightupbull);
+                                }
+                            }
+
+                            foreach (var y in reportValues)
+                            {
+                                Console.WriteLine(string.Format("{0} was purchased with ${1}.00 in revenue.", y.Key, y.Value));
+                            }
+                            break;
+                        case "2":
+                            cs.CommandText = "SELECT * FROM [BangProductRevenueReports].[dbo].[Revenue]";
+                            //
+                            cs.Connection.Open();
+                            reader = cs.ExecuteReader();
+                            while (reader.Read())
+                            {
+                                var hhh = reader[1];
+                                var o = hhh.ToString();
+                                var t = reader[3];
+
+
+                                var c = t.ToString();
+                                //  h
+                                var i = int.Parse(c);
+                                var k = reader[9];
+                                var e = k.ToString();
+                                var n = DateTime.Parse(e);
+                                var s = DateTime.Today.AddDays(-30);
+
+
+
+                                if (n > s)
+                                {
+                                    reportValues.Add(new KeyValuePair<string, int>(o, i));
+                                }
+                            }
+
+                            foreach (var y in reportValues)
+                            {
+                                var z = y.Key;
+                                var x = y.Value;
+                                var str = string.Format("{0} was purchased with ${1}.00 in revenue.", z, x);
+                                Console.WriteLine(str);
+                            }
+                            break;
+                        case "3":
+                            var h = DateTime.Today.AddDays(-90);
+
+                            cs.CommandText = "SELECT * FROM [BangProductRevenueReports].[dbo].[Revenue] WHERE PurchaseDate >= " + h;
+                            cs.Connection.Open();
+                            reader = cs.ExecuteReader();
+                            while (reader.Read())
+                            {
+                                var i = reader[1];
+                                var l = i.ToString();
+                                var o = reader[3];
+                                var v = o.ToString();
+                                var e = int.Parse(v);
+                                reportValues.Add(new KeyValuePair<string, int>(l, e));
+                            }
+
+                            foreach (var val in reportValues)
+                            {
+                                Console.WriteLine(string.Format("{0} was purchased with ${1}.00 in revenue.", val.Key, val.Value));
+                            }
+                            break;
+                        case "4":
+                            cs.CommandText = string.Format("SELECT * FROM [BangProductRevenueReports].[dbo].[Revenue]");
+                            cs.Connection.Open();
+                            reader = cs.ExecuteReader();
+                            //LIST DOESN'T WORK NEED DICTIONARY TO CHANGE VALUES
+                            Dictionary<string, int> customerReportValues = new Dictionary<string, int>();
+                            while (reader.Read())
+                            {
+                                if (customerReportValues.ContainsKey(reader[4].ToString()))
+                                {
+                                    customerReportValues[reader[4].ToString()] += int.Parse(reader[3].ToString());
+                                }
+                                else
+                                {
+                                    customerReportValues.Add(reader[4].ToString(), int.Parse(reader[3].ToString()));
+                                }
+                            }
+
+                            foreach (var val in customerReportValues)
+                            {
+                                Console.WriteLine(string.Format("{0} purchased items with a total of ${1}.00 in revenue.", val.Key, val.Value));
+                            }
+                            break;
+                        case "5":
+                            cs.CommandText = string.Format("SELECT * FROM [BangProductRevenueReports].[dbo].[Revenue]");
+                            cs.Connection.Open();
+                            reader = cs.ExecuteReader();
+
+                            //THERE HAS TO BE A BETTER WAY TO SORT
+                            Dictionary<string, int> productRevenue = new Dictionary<string, int>();
+                            SortedList<int, string> sortProductRevenue = new SortedList<int, string>();
+                            while (reader.Read())
+                            {
+                                if (productRevenue.ContainsKey(reader[1].ToString()))
+                                {
+                                    productRevenue[reader[1].ToString()] += int.Parse(reader[3].ToString());
+                                }
+                                else
+                                {
+                                    productRevenue.Add(reader[1].ToString(), int.Parse(reader[3].ToString()));
+                                }
+                            }
+                            foreach (var entry in productRevenue)
+                            {
+                                sortProductRevenue.Add(entry.Value, entry.Key);
+                            }
+                            foreach (var entry in sortProductRevenue)
+                            {
+                                Console.WriteLine(string.Format("Product: {0} Revenue: {1}", entry.Value, entry.Key));
+                            }
+
+                            //JUST IN CASE SORTING DOESN"T WORK
+                            /*Dictionary<string, int> productsReportValues = new Dictionary<string, int>();
+                            while (reader.Read())
+                            {
+                               
+                                //Dictionary<string, int> productsReportValues = new Dictionary<string, int>();
+                                if (productsReportValues.ContainsKey(reader[1].ToString()))
+                                {
+                                    productsReportValues[reader[1].ToString()] += int.Parse(reader[3].ToString());
+                                }
+                                else
+                                {
+                                    productsReportValues.Add(reader[1].ToString(), int.Parse(reader[3].ToString()));
+                                }
+                            }
+
+                            foreach (var val in productsReportValues)
+                            {
+                                Console.WriteLine(string.Format("{0} brought in a total of ${1}.00 in revenue.", val.Key, val.Value));
+                            }*/
+                            break;
+                        default:
+                            Console.WriteLine("Invalid input. Try Again.");
+                            break;
+                    }
+                    Console.ReadKey();
+                }
+                catch (Exception e)
+                {
+                    //ADDING ERROR HANDLING
+                    Console.WriteLine("Sorry an error has occcured. Please try agin ");
+                    go_on = false;
+                    Console.ReadKey();
+                }
+
+            }
         }
     }
 }
+
